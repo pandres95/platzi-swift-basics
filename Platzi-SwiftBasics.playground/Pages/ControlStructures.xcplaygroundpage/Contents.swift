1d1f1c007cfc5569875d956828a85239daa8da8f
Pablo Andrés Dorado Suárez <im@pandres95.me>
2017-12-29 12:03:43 -0500

    - Typo / Add author links - Added switch structure

--- Platzi-SwiftBasics.playground/Pages/ControlStructures.xcplaygroundpage/Contents.swift
+++ Platzi-SwiftBasics.playground/Pages/ControlStructures.xcplaygroundpage/Contents.swift
@@ -1,4 +1,4 @@
-//: # Swift Basics by Platzi (redacted by @pandres95)
+//: # Swift Basics by [Platzi](https://platzi.com/) (playground authored by [@pandres95](https://github.com/pandres95))
 //: ## Chapter 2: Control Structures
 //: ### 1. If Structure
 //: ```
@@ -37,4 +37,48 @@ repeat {
     print(j)
     j = j + 1
 } while j <= 10
-//: Back: [Variables](Variables)
+//: ### 3. For Structure
+//: ```
+//: for <#item#> in <#items#> {
+//:     <#code#>
+//: }
+//: ```
+//: #### a. For item in a range
+for i in 1...100 {
+    print(i)
+}
+//: What if we don't use the `item` variable? Use an _underscore (\_)_
+for _ in 1...10 {
+    print ("texto")
+}
+//: ### 4. Switch Structure
+//: ```
+//: switch <#value#> {
+//: case <#pattern#>:
+//:     <#code#>
+//: default:
+//:     <#code#>
+//: }
+//: ```
+var dayOfWeek = "tuesday"
+switch dayOfWeek {
+case "thursday":
+    print("Today's thursday")
+case "tuesday":
+    print("Today's tuesday")
+default:
+    print("\(dayOfWeek)'s not a valid day")
+}
+//: In case there's not a valid option, the `default` block will be executed
+dayOfWeek = "weekend"
+switch dayOfWeek {
+case "thursday":
+    print("Today's thursday")
+case "tuesday":
+    print("Today's tuesday")
+default:
+    print("It's not a valid day")
+}
+//: Previous page: [Variables](Variables)
+//:
+//: Next page: [Collections](Collections)
